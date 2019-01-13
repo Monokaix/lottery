c9c26245adac61dcfa8d5c21d67f2cf41cdb729c
Monokaix <2536818783@qq.com>
2019-01-13 12:28:31 +0800

    Add web main

---
+++ web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"myproject/lottery/bootstrap"
+	"myproject/lottery/web/routes"
+	"fmt"
+)
+
+var port = 8080
+
+ func newApp() *bootstrap.Bootstrapper {
+	 app := bootstrap.New("Go抽奖系统",
+		 "Monokaix")
+	 app.Bootstrap()
+	 app.Configure(identity.Configure, routes.Configure)
+	 return app
+ }
+
+ func main(){
+ 	app := newApp()
+ 	app.Listen(fmt.Sprintf("%d", port))
+ }
