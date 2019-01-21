15659cfcd6d02f33b86b707de0eb9c6dc227e84f
Monokaix <2536818783@qq.com>
2019-01-21 22:00:50 +0800

    Set bootstrap

--- bootstrap/bootstrapper.go
+++ bootstrap/bootstrapper.go
@@ -1,92 +1,155 @@
 package bootstrap
 
 import (
-	"github.com/kataras/iris"
 	"time"
-	"myproject/lottery/conf"
-	"github.com/kataras/iris/middleware/logger"
+
+	"github.com/gorilla/securecookie"
+	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/recover"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/sessions"
+
+	"myproject/lottery/conf"
+	"myproject/lottery/cron"
 )
 
-type Configurator func(bootstrapper *Bootstrapper)
+type Configurator func(*Bootstrapper)
 
+// 使用Go内建的嵌入机制(匿名嵌入)，允许类型之前共享代码和数据
+// （Bootstrapper继承和共享 iris.Application ）
+// 参考文章： https://hackthology.com/golangzhong-de-mian-xiang-dui-xiang-ji-cheng.html
 type Bootstrapper struct {
 	*iris.Application
-	AppName string
-	AppOwner string
+	AppName      string
+	AppOwner     string
 	AppSpawnDate time.Time
+
+	Sessions *sessions.Sessions
 }
 
-func New(appName,appOwner string, cfgs ...Configurator) *Bootstrapper{
+// New returns a new Bootstrapper.
+func New(appName, appOwner string, cfgs ...Configurator) *Bootstrapper {
 	b := &Bootstrapper{
-		Application: nil,
-		AppName:     appName,
-		AppOwner:    appOwner,
-		AppSpawnDate: time.Time{},
+		AppName:      appName,
+		AppOwner:     appOwner,
+		AppSpawnDate: time.Now(),
+		Application:  iris.New(),
 	}
-	for _, cfg := range cfgs{
+
+	for _, cfg := range cfgs {
 		cfg(b)
 	}
 
 	return b
 }
 
-func (b *Bootstrapper) SetupViews(viewDir string) {
-	htmlEngine := iris.HTML(viewDir,".html").Layout("/shared/layout.html")
+// SetupViews loads the templates.
+func (b *Bootstrapper) SetupViews(viewsDir string) {
+	htmlEngine := iris.HTML(viewsDir, ".html").Layout("shared/layout.html")
+	// 每次重新加载模版（线上关闭它）
 	htmlEngine.Reload(true)
-	htmlEngine.AddFunc("FromUnixtimeShort", func(t int) string{
-		dt := time.Unix(int64(t),int64(0))
+	// 给模版内置各种定制的方法
+	htmlEngine.AddFunc("FromUnixtimeShort", func(t int) string {
+		dt := time.Unix(int64(t), int64(0))
 		return dt.Format(conf.SysTimeformShort)
 	})
-	htmlEngine.AddFunc("FromUnixtime", func(t int) string{
-		dt := time.Unix(int64(t),int64(0))
+	htmlEngine.AddFunc("FromUnixtime", func(t int) string {
+		dt := time.Unix(int64(t), int64(0))
 		return dt.Format(conf.SysTimeform)
 	})
 	b.RegisterView(htmlEngine)
 }
 
-func (b *Bootstrapper) SetupErrorHandlers(){
-	b.OnAnyErrorCode(func(context iris.Context) {
-		err := iris.Map{
-			"app":b.AppName,
-			"status":context.GetStatusCode(),
-			"message":context.Values().GetString("message"),
-		}
-		if jsonOutput := context.URLParamExists("json"); jsonOutput{
-			context.JSON(err)
-			return
-		}
-		context.ViewData("Err", err)
-		context.ViewData("Title", "Error")
-		context.View("shared/error.html")
+// SetupSessions initializes the sessions, optionally.
+func (b *Bootstrapper) SetupSessions(expires time.Duration, cookieHashKey, cookieBlockKey []byte) {
+	b.Sessions = sessions.New(sessions.Config{
+		Cookie:   "SECRET_SESS_COOKIE_" + b.AppName,
+		Expires:  expires,
+		Encoding: securecookie.New(cookieHashKey, cookieBlockKey),
 	})
 }
 
-func (b *Bootstrapper) Configure(cs ...Configurator){
-	for _, c := range cs{
+//// SetupWebsockets prepares the websocket server.
+//func (b *Bootstrapper) SetupWebsockets(endpoint string, onConnection websocket.ConnectionFunc) {
+//	ws := websocket.New(websocket.Config{})
+//	ws.OnConnection(onConnection)
+//
+//	b.Get(endpoint, ws.Handler())
+//	b.Any("/iris-ws.js", func(ctx iris.Context) {
+//		ctx.Write(websocket.ClientSource)
+//	})
+//}
+
+// SetupErrorHandlers prepares the http error handlers
+// `(context.StatusCodeNotSuccessful`,  which defaults to < 200 || >= 400 but you can change it).
+func (b *Bootstrapper) SetupErrorHandlers() {
+	b.OnAnyErrorCode(func(ctx iris.Context) {
+		err := iris.Map{
+			"app":     b.AppName,
+			"status":  ctx.GetStatusCode(),
+			"message": ctx.Values().GetString("message"),
+		}
+
+		if jsonOutput := ctx.URLParamExists("json"); jsonOutput {
+			ctx.JSON(err)
+			return
+		}
+
+		ctx.ViewData("Err", err)
+		ctx.ViewData("Title", "Error")
+		ctx.View("shared/error.html")
+	})
+}
+
+// Configure accepts configurations and runs them inside the Bootstraper's context.
+func (b *Bootstrapper) Configure(cs ...Configurator) {
+	for _, c := range cs {
 		c(b)
 	}
 }
 
-func (b *Bootstrapper) setupCron(){
-	// TODO:
+// 启动计划任务服务
+func (b *Bootstrapper) setupCron() {
+	// 服务类应用
+	if conf.RunningCrontabService {
+		cron.ConfigueAppOneCron()
+	}
+	cron.ConfigueAppAllCron()
 }
 
 const (
-	StaticAssets = "./piblic/"
+	// StaticAssets is the root directory for public assets like images, css, js.
+	StaticAssets = "./public/"
+	// Favicon is the relative 9to the "StaticAssets") favicon path for our app.
 	Favicon = "favicon.ico"
 )
 
+// Bootstrap prepares our application.
+//
+// Returns itself.
 func (b *Bootstrapper) Bootstrap() *Bootstrapper {
 	b.SetupViews("./views")
+	b.SetupSessions(24*time.Hour,
+		[]byte("the-big-and-secret-fash-key-here"),
+		[]byte("lot-secret-of-characters-big-too"),
+	)
 	b.SetupErrorHandlers()
-	b.Favicon(StaticAssets+Favicon)
+
+	// static files
+	b.Favicon(StaticAssets + Favicon)
+	b.StaticWeb(StaticAssets[1:len(StaticAssets)-1], StaticAssets)
+
+	// crontab
 	b.setupCron()
+
+	// middleware, after static files
 	b.Use(recover.New())
 	b.Use(logger.New())
+
 	return b
 }
 
-func (b *Bootstrapper) Listen(addr string, cfgs...iris.Configurator) {
+// Listen starts the http server with the specified "addr".
+func (b *Bootstrapper) Listen(addr string, cfgs ...iris.Configurator) {
 	b.Run(iris.Addr(addr), cfgs...)
 }
